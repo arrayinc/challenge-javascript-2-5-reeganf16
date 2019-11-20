9f8adef3fb1e4e97572773371bb9bb273d40cd44
reeganford <rmford1695@gmail.com>
2019-11-20 16:15:57 -0700

    Finished challenge #5

--- answer.js
+++ answer.js
@@ -1,1 +1,21 @@
 // *** YOUR ANSWER BELOW ***
+
+function negate(str){
+    return "un" + str;
+}
+
+console.log(negate("cold"));
+
+function intensify(str){
+    return "plus" + str;
+}
+
+console.log(intensify("cold"));
+
+function reinforce(str){
+    return "double" + str;
+}
+
+console.log(reinforce("cold"));
+
+console.log(reinforce(intensify(negate("cold"))));
